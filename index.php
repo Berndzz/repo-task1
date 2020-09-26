7ed4567b42de17f17b39d4f4e0c26b1373e5f89c
Berndzz <tukanbernad@gmail.com>
2020-09-27 01:39:32 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=p, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, pariatur? Praesentium sit, magnam vero ratione est veniam repellat odio voluptatibus accusantium sint. Ipsa alias tempora in consectetur? Culpa, maxime commodi.</p>
+</body>
+</html>
